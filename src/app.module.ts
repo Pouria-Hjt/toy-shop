cdc1aa9a9b90a1176967dd60cbe48cb984345c6b
Paniz Shaabanpour <panizshaabanpour16@gmail.com>
2024-03-03 13:32:49 +0330

    docs: folders for product feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { ProductService } from './product/product.service';
+import { ProductController } from './product/product.controller';
+import { ProductModule } from './product/product.module';
+import { AdminService } from './admin/admin.service';
+import { AdminController } from './admin/admin.controller';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, ProductModule, AdminModule],
+  controllers: [AppController, UserController, ProductController, AdminController],
+  providers: [AppService, UserService, ProductService, AdminService],
 })
 export class AppModule {}
